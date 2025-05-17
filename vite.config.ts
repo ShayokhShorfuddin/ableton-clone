94f9dc137132548a3aa052ef1538f7ebafb79e92
Shayokh <shayokhshuvro14@gmail.com>
2025-05-17 11:49:08 +0600

    add homepage and base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ableton-clone",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
